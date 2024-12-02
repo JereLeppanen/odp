7359f2b9206c4fe78af20d73406fc35cb1fb506d
Jere Leppänen <jere.leppanen@nokia.com>
2024-12-02 15:21:26 +0200

    api: get_log_fn

--- include/odp/api/spec/init.h
+++ include/odp/api/spec/init.h
@@ -410,9 +410,8 @@ void odp_log_thread_fn_set(odp_log_func_t func);
  * Returns the function previously set by the calling thread via
  * odp_log_thread_fn_set(). If no thread specific log function has been set,
  * returns the log function specified in odp_init_global(). If no log function
- * was specified in odp_init_global(), or if odp_init_global() has not been
- * called yet, returns the default or override log function (see
- * odp_override_log()). Always returns a valid log function, never NULL.
+ * was specified in odp_init_global(), returns the default or override log
+ * function (see odp_override_log()). Returns NULL if ODP is not initialized.
  *
  * @return Log function
  */
